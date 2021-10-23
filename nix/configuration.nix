aac1a8fe70274681eeb9b802e997512359545a50
Konstantin Bogdnaov <var1able@var1able.ru>
2021-10-24 01:54:38 +0300

    Setup local NFS server

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -22,8 +22,9 @@
     hostName = "opti";
     enableIPv6 = false;
     firewall = {
-      allowedTCPPorts = [ 6443 ];
-    };
+      allowedTCPPorts = [ 6443 6881 2049 111 2049 4000 4001 4002 20048 32400 32080 32443 ];
+      allowedUDPPorts = [ 6881 111 2049 4000 4001 4002 20048 ];
+    };    
   };
   networking.useDHCP = false;
   networking.interfaces.enp2s0.useDHCP = true;
@@ -41,13 +42,13 @@
       fsType = "btrfs";
     };
 
-    "/mnt/kubernetes-pvs/sanic/test-pv" = {
-      device = "/mnt/sanic/test-pv";
+    "/srv/nfs/kubernetes-pvs/sanic" = {
+      device = "/mnt/sanic";
       options = [ "bind" ];
     };
 
-    "/mnt/kubernetes-pvs/chonker/torrents" = {
-      device = "/mnt/chonker/torrents";
+    "/srv/nfs/kubernetes-pvs/chonker" = {
+      device = "/mnt/chonker";
       options = [ "bind" ];
     };
   };
@@ -62,7 +63,16 @@
 
   services.k3s = {
     enable = true;
-    extraFlags = "--tls-san k8s.home.var1able.ru";
+    extraFlags = "--tls-san k8s.home.var1able.ru --disable traefik --disable local-storage";
+  };
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /srv/nfs/kubernetes-pvs/chonker 127.0.0.0/8(rw,sync,all_squash,no_subtree_check,anonuid=1000,anongid=100) 192.168.103.0/24(rw,insecure,sync,all_squash,no_subtree_check,anonuid=1000,anongid=100)
+      /srv/nfs/kubernetes-pvs/sanic 127.0.0.0/8(rw,sync,all_squash,no_subtree_check,anonuid=1000,anongid=100)
+    '';
+    createMountPoints = true;
   };
 
   system.stateVersion = "21.05"; # Did you read the comment?
